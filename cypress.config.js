5ba9cf03cbd302d280e3046ca855fb617a546555
Chakra <ballachakri@yahoo.com>
2026-07-10 15:29:49 +0100

    redone the ci code

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: "mochawesome",
-
   reporterOptions: {
+    reportDir: "cypress/reports",
     overwrite: false,
     html: false,
-    json: true,
+    json: true
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      return config;
-    },
-  },
+    baseUrl: "http://localhost:3000",
+    video: true,
+    screenshotOnRunFailure: true
+  }
 });
